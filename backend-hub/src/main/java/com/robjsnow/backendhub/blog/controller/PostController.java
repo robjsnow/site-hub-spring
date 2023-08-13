0cc3fcdee5fde517966e4f210fc669833e6a2581
Robert Snow <robjsnow@gmail.com>
2023-08-13 15:17:06 -0500

    added cors fix

--- backend-hub/src/main/java/com/robjsnow/backendhub/blog/controller/PostController.java
+++ backend-hub/src/main/java/com/robjsnow/backendhub/blog/controller/PostController.java
@@ -3,6 +3,7 @@ import com.robjsnow.backendhub.blog.model.Post;
 import com.robjsnow.backendhub.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -28,12 +29,14 @@ public class PostController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-
+    
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MODERATOR')")
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
         return ResponseEntity.ok(postService.savePost(post));
     }
 
+ 
     @PutMapping("/{id}")
     public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post post) {
         postService.getPostById(id).orElseThrow(() -> new RuntimeException("Post not found"));
@@ -41,6 +44,7 @@ public class PostController {
         return ResponseEntity.ok(postService.savePost(post));
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MODERATOR')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable Long id) {
         postService.deletePost(id);
